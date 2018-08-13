1dfee7383dbbcb409c985fb3a4462344fb5e56b8
anaidenko <Andrii.Naidenko@outlook.com>
2018-08-13 13:16:22 +0200

    Clean up, add prettier, autoformat code.

--- index.ts
+++ index.ts
@@ -1,24 +1,24 @@
-import * as fs from "fs";
+import * as fs from 'fs';
 import * as path from 'path';
-import * as cheerio from "cheerio";
+import * as cheerio from 'cheerio';
 import csvStringify from 'csv-stringify';
-const tika = require("tika");
+const tika = require('tika');
 const tabula = require('./tabula-js/index');
 
-import { ExtractedData } from "./types";
+import { ExtractedData } from './types';
 import { ESClient } from './elasticsearch';
 
-// const FILEPATH = "./docs/SMR Policy.pdf";
-// const FILEPATH = "./docs/MIFID II.pdf";
-const FILEPATH = "./docs/Twitter 10K Dec 2015.pdf";
-// const FILEPATH = "./docs/ast_sci_data_tables_sample.pdf";
+// const FILEPATH = './docs/SMR Policy.pdf';
+// const FILEPATH = './docs/MIFID II.pdf';
+const FILEPATH = './docs/Twitter 10K Dec 2015.pdf';
+// const FILEPATH = './docs/ast_sci_data_tables_sample.pdf';
 
 run();
 
-let pageCount = 0
-let paragraphCount = 0
-let sentenceCount = 0
-let tableCount = 0
+let pageCount = 0;
+let paragraphCount = 0;
+let sentenceCount = 0;
+let tableCount = 0;
 
 async function run() {
   try {
@@ -28,19 +28,19 @@ async function run() {
     }
 
     const content = await extractContent(FILEPATH);
-    const esItem = await indexContentBySegments(content, 'tika-file', FILEPATH)
-    console.log('ES index response', esItem)
+    const esItem = await indexContentBySegments(content, 'tika-file', FILEPATH);
+    console.log('ES index response', esItem);
 
-    if (content.meta["Content-Type"] === "application/pdf") {
-      const tabulaResponse = await extractTablesFromPdf(FILEPATH)
+    if (content.meta['Content-Type'] === 'application/pdf') {
+      const tabulaResponse = await extractTablesFromPdf(FILEPATH);
       const htmlTables = await Promise.all(tabulaResponse.map(table => tabulaDataToHtml(table.data)));
-      const csvTables = await Promise.all(tabulaResponse.map(table => tabulaDataToCsv(table.data)));
-      console.log("Extracted tables", csvTables);
+      // const csvTables = await Promise.all(tabulaResponse.map(table => tabulaDataToCsv(table.data)));
+      // console.log("Extracted tables", csvTables);
       const response = await indexTables(esItem, htmlTables);
-      console.log('ES tables index response', response)
+      console.log('ES tables index response', response);
     }
   } catch (err) {
-    console.error("Error", err);
+    console.error('Error', err);
   }
 }
 
@@ -53,23 +53,22 @@ async function extractContent(filepath) {
         const $head = $('head');
         const $body = $('body');
 
-        let meta = {}
-        meta['title'] = $head.find('title').html()
+        let meta = {};
+        meta['title'] = $head.find('title').html();
         $head.find('meta[name]').each((i, el) => {
-          const $meta = $(el)
-          const name = $meta.attr('name')
-          const content = $meta.attr('content')
-          meta[name] = content
-        })
+          const $meta = $(el);
+          const name = $meta.attr('name');
+          const content = $meta.attr('content');
+          meta[name] = content;
+        });
 
-        $body.find('p').filter((i, p) => ($(p).html() || '').trim() === '').remove()
+        $body
+          .find('p')
+          .filter((i, p) => ($(p).html() || '').trim() === '')
+          .remove();
         // $body.find('.page').filter((i, page) => page.children.length === 0).remove()
-        
-        let html = $body.html() || '';
 
-        // if ($body.find('a').length === 0) {
-        //     html = html.replace(/\b(https?:\/\/[^\b\s ]+)\b/g, '<a href="$1">$1</a>');
-        // }
+        let html = $body.html() || '';
 
         resolve({
           $: $,
@@ -82,108 +81,83 @@ async function extractContent(filepath) {
   });
 }
 
-async function indexContent(content: ExtractedData, es_index: string, filepath: string) {
-  return new Promise((resolve, reject) => {
-    const client = new ESClient().client();
-    const filesize = fs.statSync(filepath).size
-    client.index(
-      {
-        index: es_index,
-        type: "file",
-        id: path.basename(filepath), // todo: remove
-        requestTimeout: 1000 * 60 * 60 * 24,
-        body: {
-          timestamp: new Date().toISOString(),
-          username: "admin",
-          filename: path.basename(FILEPATH),
-          folder: "",
-          attachment: {
-            date: content.meta["date"],
-            title: content.meta["title"],
-            // language: 'en', // tika.xhtml doesn't detect document language, try tika.language instead
-            keywords: content.meta["keywords"],
-            content_length: filesize,
-            content_type: content.meta["Content-Type"],
-            content: content.html
-          }
-        }
-      },
-      (err, response) => {
-        if (err) reject(err);
-        else resolve(response);
-      }
-    );
-  })
-}
-
-async function indexContentBySegments(
-  content: ExtractedData,
-  es_index: string,
-  filepath: string
-) {
+async function indexContentBySegments(content: ExtractedData, es_index: string, filepath: string) {
   return new Promise((resolve, reject) => {
     const client = new ESClient().client();
     const filesize = fs.statSync(filepath).size;
-    const $ = content.$
+    const $ = content.$;
 
-    let pages = content.$html.find('.page').toArray().map(page => $(page).children().length === 0 ? '' : $.html(page)).filter(page => page !== '')
-    // pages = pages.map(page => page.replace(/\n*(<p>)\n*/g, '\n$1').replace(/(<\/?(p|div)[^>]*>)/gi, "")); // strip html tags
+    let pages = content.$html
+      .find('.page')
+      .toArray()
+      .map(page => ($(page).children().length === 0 ? '' : $.html(page)))
+      .filter(page => page !== '');
 
-    // const whitelistTags = "table|th|tr|td|b|i|u|strong|ul|ol|li|small|sub|sup|em|span|font|big|del|s|strike|tt|blockquote".split("|");
-    // pages = pages.map(page => page.replace(/\n*(<p>)\n*/g, '\n$1').replace(new RegExp('<(?!\/?(' + whitelistTags.join('|') + '))[^>]+>/g', 'gi'), "")); // strip html tags except tables
+    let tables = content.$html
+      .find('table')
+      .toArray()
+      .map(table => ({
+        id: ++tableCount,
+        content: $.html(table)
+      }));
 
-    let tables = content.$html.find('table').toArray().map(table => ({
-      id: ++tableCount,
-      content: $.html(table)
-    }))
-
-    const doc = {
+    const document = {
       content: formatHtml(content.html),
       pages: pages.map(page => {
-        page = formatHtml(page).trim()
-        let paragraphs3 = page.split(/<p[^>]*>/gi).map(page => page.replace(/<\/?(p|div)[^>]*>/gi, '')).filter(p => p !== '');
+        page = formatHtml(page).trim();
+
+        let paragraphs = page
+          .split(/<p[^>]*>/gi)
+          .map(page => page.replace(/<\/?(p|div)[^>]*>/gi, ''))
+          .filter(p => p !== '');
+
         return {
           id: ++pageCount,
           content: page,
-          paragraphs: paragraphs3.map(p => {
-            p = p.trim()
-            const sentences3 = p.replace(/\n+/g, "").replace(/([.|!|?]+)/g, '$1\n').split('\n');
+          paragraphs: paragraphs.map(paragraph => {
+            paragraph = paragraph.trim();
+
+            const sentences = paragraph
+              .replace(/\n+/g, '')
+              .replace(/([.|!|?]+)/g, '$1\n')
+              .split('\n');
+
             return {
               id: ++paragraphCount,
-              content: p,
-              sentences: sentences3.map(sentence => {
-                sentence = sentence.trim()
+              content: paragraph,
+              sentences: sentences.map(sentence => {
+                sentence = sentence.trim();
                 return {
                   id: ++sentenceCount,
                   content: sentence
-                }
+                };
               })
-            }
+            };
           })
-        }
+        };
       }),
       tables: tables
-    }
+    };
 
     client.index(
       {
         index: es_index,
-        type: "file",
+        type: 'file',
         id: path.basename(filepath), // todo: remove
         requestTimeout: 1000 * 60 * 60 * 24,
         body: {
           timestamp: new Date().toISOString(),
-          username: "admin",
+          username: 'admin',
           filename: path.basename(FILEPATH),
-          folder: "",
+          folder: '',
           attachment: {
-            date: content.meta["date"],
-            title: content.meta["title"],
+            date: content.meta['date'],
+            title: content.meta['title'],
             // language: 'en', // todo: detect language separately (tika.language command) as tika.xhtml doesn't do that for us
-            keywords: content.meta["keywords"],
+            keywords: content.meta['keywords'],
             content_length: filesize,
-            content_type: content.meta["Content-Type"],
-            ...doc
+            content_type: content.meta['Content-Type'],
+            ...document
           }
         }
       },
@@ -198,8 +172,8 @@ async function indexContentBySegments(
 function formatHtml(html) {
   if (!html) return html;
   let result = html;
-  result = result.replace(/([^.?!])\s*<\/p>\s*\n\s*<p>\s*([a-z&])/gi, "$1 $2"); // remove newlines from paragraphs
-  result = result.replace(/&#xA0;/gi, " ").replace(/&#x2019;/gi, "'");
+  result = result.replace(/([^.?!])\s*<\/p>\s*\n\s*<p>\s*([a-z&])/gi, '$1 $2'); // remove newlines from paragraphs
+  result = result.replace(/&#xA0;/gi, ' ').replace(/&#x2019;/gi, "'");
   return result;
 }
 
@@ -207,37 +181,52 @@ async function extractTablesFromPdf(filepath: string) {
   return new Promise((resolve: (data: any[]) => void, reject) => {
     // todo: impelement RESTful wrapper around tabula
     tabula(path.resolve(filepath), { guess: '', silent: '', pages: 'all', format: 'JSON' }).extractCsv((err, data) => {
-      if (err) reject(err)
-      else resolve(JSON.parse(data))
-    })
+      if (err) reject(err);
+      else resolve(JSON.parse(data));
+    });
   });
 }
 
 async function indexTables(esItem: any, csvTables: string[]) {
   return new Promise((resolve, reject) => {
-    if (csvTables.length === 0) return resolve()
+    if (csvTables.length === 0) return resolve();
 
     const client = new ESClient().client();
-    client.update({
-      index: esItem._index,
-      type: "file",
-      id: esItem._id,
-      requestTimeout: 1000 * 60 * 60 * 24,
-      body: {
-        doc: {
-          attachment: {
-            tables: csvTables.map(csv => ({
-              id: ++tableCount,
-              content: csv
-            }))
+    client.update(
+      {
+        index: esItem._index,
+        type: 'file',
+        id: esItem._id,
+        requestTimeout: 1000 * 60 * 60 * 24,
+        body: {
+          doc: {
+            attachment: {
+              tables: csvTables.map(csv => ({
+                id: ++tableCount,
+                content: csv
+              }))
+            }
           }
         }
+      },
+      (err, response) => {
+        if (err) reject(err);
+        else resolve(response);
       }
-    }, (err, response) => {
-      if (err) reject(err);
-      else resolve(response);
-    })
-  })
+    );
+  });
+}
+
+async function tabulaDataToHtml(result) {
+  return new Promise((resolve: (csv: string) => void, reject) => {
+    const data = result.map(row => row.map(cell => cell.text));
+
+    let rows = [];
+    rows = [...rows, data.map(row => '<tr>' + row.map(cell => '<td>' + cell + '</td>').join('') + '</tr>')];
+    let table = '<table>\n' + rows.join('\n') + '</table>';
+
+    resolve(table);
+  });
 }
 
 async function tabulaDataToCsv(result) {
@@ -253,17 +242,5 @@ async function tabulaDataToCsv(result) {
         else resolve(response);
       }
     );
-  })
-}
-
-async function tabulaDataToHtml(result) {
-  return new Promise((resolve: (csv: string) => void, reject) => {
-    const data = result.map(row => row.map(cell => cell.text));
-
-    let rows = []
-    rows = [...rows, data.map(row => '<tr>' + row.map(cell => '<td>' + cell + '</td>').join('') + '</tr>')]
-    let table = '<table>\n' + rows.join('\n') + '</table>'
-
-    resolve(table)
-  })
+  });
 }
